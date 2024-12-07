ef5b825c13dd20ce32ea2b60d93eab91c26c0f2a
zalab-inc <55264582+zalab-inc@users.noreply.github.com>
2024-12-08 04:59:05 +0700

    Update home page layout and content

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,32 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </main>
-  );
+export default async function HomePage() {
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
+	if (!session) {
+		redirect("/login");
+	} else {
+		redirect("/dash");
+	}
+  
+
+	// return (
+	// 	<main className="flex flex-col justify-center flex-1 text-center">
+	// 		<h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+	// 		<p className="text-fd-muted-foreground">
+	// 			You can open{" "}
+	// 			<Link
+	// 				href="/docs"
+	// 				className="font-semibold underline text-fd-foreground"
+	// 			>
+	// 				/docs
+	// 			</Link>{" "}
+	// 			and see the documentation.
+	// 		</p>
+	// 	</main>
+	);
 }
