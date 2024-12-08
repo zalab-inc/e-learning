53db01aee1407a08acd6b34422616588221c18d1
zalab-inc <55264582+zalab-inc@users.noreply.github.com>
2024-12-08 08:31:28 +0700

    fix: update build script and clean up layout component by removing unused sidebar elements

--- app/course/layout.tsx
+++ app/course/layout.tsx
@@ -3,33 +3,11 @@ import type { ReactNode } from "react";
 import { source } from "@/lib/source";
 import { RootProvider } from "fumadocs-ui/provider";
 import { baseOptions } from "@/app/layout.config";
-import { RootToggle } from "fumadocs-ui/components/layout/root-toggle";
 
 export default async function Layout({ children }: { children: ReactNode }) {
 	return (
 		<RootProvider>
-			<DocsLayout
-				tree={source.pageTree}
-				{...baseOptions}
-				sidebar={{
-					banner: (
-						<RootToggle
-							options={[
-								{
-									title: "Folder 1",
-									description: "Pages in folder 1",
-									url: "/path/to/page-tree-1",
-								},
-								{
-									title: "Folder 2",
-									description: "Pages in folder 2",
-									url: "/path/to/page-tree-2",
-								},
-							]}
-						/>
-					),
-				}}
-			>
+			<DocsLayout tree={source.pageTree} {...baseOptions}>
 				{children}
 			</DocsLayout>
 		</RootProvider>
